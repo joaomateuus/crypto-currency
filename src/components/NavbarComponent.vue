ecc2901b3e713f0f329b27921a82ca4bcaae6b5b
João Mateus Morais de Amaral <02758621274@imprensa.br>
2022-03-08 09:35:52 -0400

    commiting

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-toolbar flat>
+      <v-toolbar-title>Cripto Currency</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>Home</v-btn>
+
+        <v-btn text>Currency</v-btn>
+
+        <v-btn text>News</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'NavbarComponent',
+
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeApp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav-bar">
+    <NavbarComponent />
+  </div>
+
+
+</template>
+
+<script>
+import NavbarComponent from '../components/NavbarComponent.vue'
+
+export default {
+  name: 'HomeApp',
+  components: 
+  {
+   NavbarComponent,
+  }
+}
+</script>
+
+
+<style scoped>
+
+
+
+
+</style>
